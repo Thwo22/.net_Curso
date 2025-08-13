4282d4e36d72da2b60de6915a7652ef689a03a17
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-08-13 19:09:30 -0300

    3.2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,140 @@
-﻿using models;
+﻿using System.Diagnostics;
+using models;
 
-bool choveu = false;
-bool estaTarde = false;
+string opcao;
+bool exibir = true;
+while (exibir)
+{
+    Console.Clear();
+    Console.WriteLine("Digite sua opção:");
+    Console.WriteLine("1 - Cadastrar Cliente");
+    Console.WriteLine("2 - Buscar Cliente");
+    Console.WriteLine("3 - Apagar Cliente:");
+    Console.WriteLine("4 - Encerrar");
 
-if (!choveu && !estaTarde)
-{
-    Console.WriteLine("pode ir pedalar");
-}
-else
-{
-    Console.WriteLine("Não pode ir pedalar");
-}
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+
+        case "2":
+            Console.WriteLine("Busca de Cliente");
+            break;
+
+        case "3":
+            Console.WriteLine("Apagar Cliente");
+            break;
+
+        case "4":
+            Console.WriteLine("Encerrar");
+            exibir = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção Inválida");
+            break;
+    }
+
+    Console.WriteLine("O Programa Se Encerrou");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int numero = 5;
+// int contador = 0;
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($" {contador}* Execução: {numero} x {contador} = {numero * contador}");
+//     contador++;
+// }
+
+
+
+// int numero = 5;
+
+// for(int contador = 0; contador <= 10; contador++)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Calculadora calc = new Calculadora();
+// calc.RaizQuadrada(10);
+
+// calc.Somar(10, 30);
+// calc.Subtrair(10, 50);
+// calc.Multiplicar(15, 45);
+// calc.Divisao(2, 2);
+// calc.Potencia(3, 3);
+
+// bool choveu = false;
+// bool estaTarde = false;
+
+// if (!choveu && !estaTarde)
+// {
+//     Console.WriteLine("pode ir pedalar");
+// }
+// else
+// {
+//     Console.WriteLine("Não pode ir pedalar");
+// }
+// int numero = 10;
+
+// Console.WriteLine(numero);
+
+// Console.WriteLine("Incrementando o 10");
+// numero++;
+
+// Console.WriteLine(numero);
---
+++ models/Calculadora.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace models
+{
+//     public class Calculadora
+//     {
+
+
+//         public void Somar(int x, int y)
+//         {
+//             Console.WriteLine($"{x} + {y} é igual a: {x + y}");
+//         }
+
+//         public void Subtrair(int x, int y)
+//         {
+//             Console.WriteLine($"{x} - {y} é igual a: {x - y}");
+//         }
+
+//         public void Multiplicar(int x, int y)
+//         {
+//             Console.WriteLine($"{x} x {y} é igual a: {x * y}");
+//         }
+
+//         public void Divisao(int x, int y)
+//         {
+//             Console.WriteLine($"{x} / {y} é igual a: {x / y}");
+//         }
+//         public void Potencia(int x, int y)
+//         {
+//             double pot = Math.Pow(x, y);
+//             Console.WriteLine($"{x} Elevado {y} é igual a: {pot}");
+//         }
+//         public void Seno(double angulo)
+//         {
+//             double radiano = angulo * Math.PI / 100;
+//             double seno = Math.Sin(radiano);
+//             Console.WriteLine($"Seno de {angulo} * = {Seno}");
+//         }
+//         public void RaizQuadrada(double x)
+//         {
+//             double raiz = Math.Sqrt(x);
+//             Console.WriteLine($"A Raiz quadrada de {x} é igual a : {raiz}");
+//         }
+//     }
+}
